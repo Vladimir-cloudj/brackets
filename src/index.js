2ca78263dd95fdc412939ab2d042ba60c768fa58
Vladimir <wizart88@gmail.com>
2022-02-23 22:41:29 +0400

    feat: solve tasks brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, BracketsConfig) {
   // your solution
+  const stack = [];
+  const arrBrackets = {};
+
+    BracketsConfig.forEach((b) => (arrBrackets[b[0]] = b[1]));
+
+    const arr = str.split("");
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arrBrackets[stack[stack.length - 1]] === arr[i]) {
+            stack.pop();
+        } else if (arrBrackets.hasOwnProperty(arr[i])) {
+            stack.push(arr[i]);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0 ? true : false;
 }
